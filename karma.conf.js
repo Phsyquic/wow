f14553d191c207d531ae4126dee9573cd5798c76
Phsyquic <phsyquic1993@gmail.com>
2024-09-28 18:58:54 +0200

    Cambio de nombre

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pkm-app'),
+      dir: require('path').join(__dirname, './coverage/wowAPP'),
       subdir: '.',
       reporters: [
         { type: 'html' },
