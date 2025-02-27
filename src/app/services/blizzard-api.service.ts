48fa67822a567df95b6f8d05d3c1ae8c88085dd7
Phsyquic <phsyquic1993@gmail.com>
2025-02-27 21:35:32 +0100

    Actualizar Bis List

--- src/app/services/blizzard-api.service.ts
+++ src/app/services/blizzard-api.service.ts
@@ -7,6 +7,8 @@ import { CacheBlizzardApiService } from './cache-blizzard-api.service';
   providedIn: 'root',
 })
 export class BlizzardApiService {
+  private serverUrl = 'http://localhost:3000';
+
   constructor(
     private http: HttpClient,
     private cacheBlizzardApiService: CacheBlizzardApiService
@@ -14,16 +16,19 @@ export class BlizzardApiService {
 
   // Obtener la imagen de un ítem
   getItemMedia(itemId: number): Observable<any> {
+    //return this.http.get(`${this.serverUrl}/item-media/${itemId}`);
     return this.cacheBlizzardApiService.getItemMedia(itemId);
   }
 
   // Obtener el nombre de un ítem
   getItemName(itemId: number): Observable<any> {
+    //return this.http.get(`${this.serverUrl}/item-name/${itemId}`, { responseType: 'json' });
     return this.cacheBlizzardApiService.getItemName(itemId);
   }
 
   // Obtener datos de un journal encounter
   getJournalEncounter(journalEncounterId: number): Observable<any> {
+    //return this.http.get(`${this.serverUrl}/journal-encounter/${journalEncounterId}`);
     return this.cacheBlizzardApiService.getJournalEncounter(journalEncounterId);
   }
 }
