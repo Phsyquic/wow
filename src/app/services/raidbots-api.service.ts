ae3ffc6f7b8c1412e31253bad1a1a4b55e56d8a9
IvanRomeroFDSA <ivan.romero@fdsa.es>
2026-03-04 12:31:07 +0100

    Cors fix

--- src/app/services/raidbots-api.service.ts
+++ src/app/services/raidbots-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -13,14 +13,10 @@ export class RaidbotsApiService {
   getDroptimizer(report: any): Observable<Object> {
     const relativeUrl = `/simbot/${report}/data.json`;
     const absoluteUrl = `https://www.raidbots.com/simbot/${report}/data.json`;
-    let header = new HttpHeaders()
-    .set('Content-Type', 'application/json')
 
-     return this.http.get(relativeUrl, {
-      headers: header
-     }).pipe(
+     return this.http.get(relativeUrl).pipe(
       // Dev local uses proxy (relative URL). Public static deploy needs absolute Raidbots URL.
-      catchError(() => this.http.get(absoluteUrl, { headers: header }))
+      catchError(() => this.http.get(absoluteUrl))
      );
   }
 
@@ -33,22 +29,12 @@ export class RaidbotsApiService {
 
   getInstances(): Observable<Object> {
     const _url = 'https://www.raidbots.com/static/data/live/instances.json';
-    let header = new HttpHeaders()
-    .set('Content-Type', 'application/json')
-
-     return this.http.get(_url, {
-      headers: header
-     });
+     return this.http.get(_url);
   }
 
   getItemList(): Observable<Object> {
     const _url = 'https://www.raidbots.com/static/data/live/encounter-items.json';
-    let header = new HttpHeaders()
-    .set('Content-Type', 'application/json')
-
-     return this.http.get(_url, {
-      headers: header
-     });
+     return this.http.get(_url);
   }
     
 }
